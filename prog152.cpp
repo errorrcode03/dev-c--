4fb0c4ccee3eb56a80d2385b8705e58f1992303a
Sayan Mistri <sayan2003mistri@gmail.com>
2026-05-23 23:12:32 +0530

    7

--- prog152.cpp
+++ prog152.cpp
@@ -1,10 +1,29 @@
 #include<iostream>
 #include<vector>
 using namespace std ;
+int Index(vector<int>& nums , int st, int end){
+	int indx = st-1, pivot = nums[end ];
+	for(int i=st;i<end ;i++){
+		if(nums[i]<=pivot){
+			indx++;
+			swap(nums[i],nums[indx]);
+		}
+	} 
+	indx++;
+	swap(nums[end ], nums[indx]);
+	return indx;
+}
 void quickSort(vector<int>& nums , int start, int end ){
     if(start<end){
-	int pivotIndx=findPivot(nums, start, end );
-	quickSort(nums, start,pivotindx-1);
+	int pivotIndx=Index(nums, start, end );
+	quickSort(nums, start,pivotIndx-1);
 	quickSort(nums, pivotIndx+1,end );
 }
+}
+int main(){
+	vector<int>vec={11,13,18,20,36,33,56,93,14};
+	quickSort(vec,0,vec.size()-1);
+	for(int val:vec){
+		cout<<val<<" ";
+	}
 }
